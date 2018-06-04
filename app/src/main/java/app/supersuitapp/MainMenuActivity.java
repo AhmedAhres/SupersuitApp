49abd8e5a63bbab170f54335a4eb86bf08ab6df9
Ahmed Ahres <ahres.ahmed@yahoo.fr>
2018-06-04 11:30:15 +0200

    Added images

--- app/src/main/java/app/supersuitapp/MainMenuActivity.java
+++ app/src/main/java/app/supersuitapp/MainMenuActivity.java
@@ -3,6 +3,7 @@ package app.supersuitapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainMenuActivity extends AppCompatActivity {
@@ -13,29 +14,39 @@ public class MainMenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
     }
 
-    ImageView button = (ImageView) findViewById(R.id.settingsButton);
+//    ImageView mImageView = (ImageView)findViewById(R.id.settingsButton);
+//    mImageView.setOnClickListener(new View.OnClickListener() {
+//
+//        @Override
+//        public void onClick(View view) {
+//            // do stuff
+//        }
+//
+//    });
 
-    /**
-     * opens the initial state and closes this state
-     */
+
+//    mImageView.setOnClickListener(new View.OnClickListener() {
+//
+//        @Override
+//        public void onClick(View view) {
+//            // do stuff
+//        }
+//
+//    });
+
+
     public void openBatteryScreen() {
         Intent intent = new Intent(this, BatteryActivity.class);
         startActivity(intent);
         finish();
     }
 
-    /**
-     * opens the initial state and closes this state
-     */
     public void openSettingsScreen() {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
         finish();
     }
 
-    /**
-     * opens the initial state and closes this state
-     */
     public void openWeightScreen() {
         Intent intent = new Intent(this, Weight.class);
         startActivity(intent);
